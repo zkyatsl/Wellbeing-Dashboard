95f8e516fbc47a22c8b26c6987aecba9f2fa50cd
Zakky <zakkyramdanie11@gmail.com>
2025-04-28 20:51:23 +0800

    update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
